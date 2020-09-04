c07d01566796342a7431dcde0ecd12b918bf7f27
unknown <saatvikrawat8921@gmail.com>
2020-09-04 17:21:34 +0530

    v1.0.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,156 +11,200 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(
-                backgroundColor: Color(0xFF1A237E),
-                centerTitle: true,
-                title: Text("BRL TRAINEE")),
-            body: Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Center(
-                    child: CircleAvatar(
-                      radius: 40,
-                      backgroundImage: AssetImage('images/profile.png'),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 7,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Center(
-                    child: Text(
-                      "Saatvik Rawat",
-                      style: TextStyle(
-                        color: Color(0x0FF000000),
-                        fontWeight: FontWeight.bold,
-                        fontStyle: FontStyle.italic,
-                        fontSize: 35,
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Center(
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(left: 25),
-                        child: Icon(
-                          Icons.school,
-                          color: Colors.black,
-                          size: 30,
+            backgroundColor: Colors.black,
+            body: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Center(
+                        child: Text(
+                          "BRL Trainee",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 35,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        "Pursuing B.Tech(2nd year) in CS",
-                        style: TextStyle(fontSize: 20),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Row(
-                  children: <Widget>[
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
                     Padding(
-                      padding: const EdgeInsets.only(left: 25.0),
-                      child: Icon(
-                        Icons.place,
-                        color: Colors.green,
-                        size: 30,
+                      padding: const EdgeInsets.all(8.0),
+                      child: Center(
+                        child: CircleAvatar(
+                            radius: 45,
+                            backgroundColor: Colors.grey,
+                            child: CircleAvatar(
+                              radius: 40,
+                              backgroundImage: AssetImage('images/profile.png'),
+                            )),
                       ),
                     ),
                     SizedBox(
-                      width: 10,
+                      height: 7,
                     ),
-                    Text(
-                      "Muzaffarnagar(U.P.),India",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Row(
-                  children: <Widget>[
                     Padding(
-                      padding: const EdgeInsets.only(left: 25),
-                      child: Icon(
-                        Icons.email,
-                        color: Colors.yellow,
-                        size: 30,
+                      padding: const EdgeInsets.all(12.0),
+                      child: Center(
+                        child: Text(
+                          "Saatvik Rawat",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontStyle: FontStyle.italic,
+                            fontSize: 35,
+                          ),
+                        ),
                       ),
                     ),
                     SizedBox(
-                      width: 10,
+                      height: 30,
+                    ),
+                    Center(
+                      child: Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(left: 25),
+                            child: Icon(
+                              Icons.school,
+                              color: Colors.grey,
+                              size: 30,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "Pursuing B.Tech(2nd year) in CS",
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(left: 25.0),
+                          child: Icon(
+                            Icons.place,
+                            color: Colors.green,
+                            size: 30,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          "Muzaffarnagar(U.P.),India",
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(left: 25),
+                          child: Icon(
+                            Icons.email,
+                            color: Colors.yellow,
+                            size: 30,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          "saatvikrawat8921@gmail.com",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 35,
+                    ),
+                    Center(
+                        child: Text(
+                      "About Me",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 28,
+                        color: Colors.white,
+                      ),
+                    )),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "I am pursuing B.Tech with CS as my branch from Ajay Kumar garg Engineering college.I have done programming in core language like C++.Now,I am learning App development using Flutter.I am innovative and has a great capability to work with a team.",
+                        textAlign: TextAlign.center,
+                        maxLines: 15,
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
                     ),
                     Text(
-                      "saatvikrawat8921@gmail.com",
-                      style: TextStyle(fontSize: 20),
+                      "Goals",
+                      style: TextStyle(
+                        fontSize: 28,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "To be a proficient programmer with excellent coding skills and a broad exposure to computer system.",
+                        textAlign: TextAlign.center,
+                        maxLines: 15,
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Center(
+                      child: RaisedButton(
+                        onPressed: _launchURL,
+                        child: Text("My Resume"),
+                      ),
                     ),
                   ],
-                ),
-                SizedBox(
-                  height: 35,
-                ),
-                Center(
-                    child: Text(
-                  "About Me",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 28,
-                  ),
-                )),
-                SizedBox(
-                  height: 5,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    "I am pursuing B.Tech with CS as my branch from Ajay Kumar garg Engineering college.I have done programming in core language like C++.Now,I am learning App development using Flutter.I am innovative and has a great capability to work with a team.",
-                    textAlign: TextAlign.center,
-                    maxLines: 6,
-                    style: TextStyle(
-                      fontSize: 15,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Text(
-                  "Goals",
-                  style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    "To be a proficient programmer with excellent coding skills and a broad exposure to computer system.",
-                    textAlign: TextAlign.center,
-                    maxLines: 3,
-                    style: TextStyle(
-                      fontSize: 15,
-                    ),
-                  ),
-                ),
-              ],
-            )));
+                ))));
+  }
+}
+
+_launchURL() async {
+  const url =
+      "https://drive.google.com/file/d/1YNKnAySQIYFVY0WLMW183K71rnDGq7Kn/view?usp=sharing";
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
